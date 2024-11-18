34961e891d3ebaf1fb5babafac5efdc36f9bd655
ulugbekbrat <168886327+ulugbekbrat@users.noreply.github.com>
2024-11-18 18:47:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// '1-savol'
+// let a = 5
+// alert(a % 2)
+
+
+
+// '2-savol'
+// let b = alert(Math.round(Math.random()*10))
+
+// '3-savol'
+// let c = confirm('12.510 butun songa aylantirasmi')
+// alert(Math.trunc(12.510))
+
+// '4-savol'
+// let d = prompt('Yoshingizni kiriting')
+// if (d == 18) {
+//     alert( "siz bilag'onsiz ekansiz" )
+    
+// }else{
+//     alert( 'siz tup ekansiz' )
+    
+// }
+
+
